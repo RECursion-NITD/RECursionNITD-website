bc7ef76eb9149aa5c6a2fb92d2219d7a7301636c
vjs2234 <jeyasurya.v@gmail.com>
2018-12-19 22:23:18 +0530

    added python social auth with github

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path,include
+from recursion_website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('oauth/', include('social_django.urls', namespace='social')), 
+    
 ]
