5d9cefb998e6bc5d4c1eae9178f3784bbf481647
pkkp86nitd <pkkumarpankaj86@gmail.com>
2018-12-27 14:52:54 +0530

    urls without regexp

--- website/recursion_website/urls.py
+++ website/recursion_website/urls.py
@@ -1,15 +1,11 @@
-
-from . import views
+from django.urls import path
+from .views import *
 from django.conf.urls import include,url
 from django.conf import settings
-from django.urls import path
-
 
 urlpatterns = [
-               
-                 url(r'add_question/$',views.add_question,name = 'add_question'),
-                 path('',views.list_questions ,name='list_questions'),
-                 path('detail/<int:id>/',views.detail_questions, name='detail_questions'),
-                 path('update/<int:id>/',views.update_questions, name='update_question'),
-               
-              ]
+    path('add', add_question, name='add_question'),
+    path('', list_questions ,name='list_questions'),
+    path('detail/<int:id>/', detail_questions, name='detail_questions'),
+    path('update/<int:id>/', update_questions, name='update_question'),
+]
