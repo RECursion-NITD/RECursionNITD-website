9bec5d10f19a3ced2e77aadd3e0d5da0aedbbe76
pkkp86nitd <pkkumarpankaj86@gmail.com>
2019-01-03 12:27:52 +0530

    ordering of list questions on forum page by created_at date

--- website/recursion_website/models.py
+++ website/recursion_website/models.py
@@ -22,6 +22,7 @@ class Questions(models.Model):
         return self.title
     class Meta:
         managed = True
+        ordering = ['-created_at']
         db_table = 'questions'
         verbose_name_plural = 'Questions'
  
