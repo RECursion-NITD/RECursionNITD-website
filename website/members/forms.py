7e17a2670070d5af85fea49a334659ee7fea1ace
Sparsh Kedia <sparsh.kedia@gmail.com>
2019-03-03 02:45:55 +0530

    Update forms.py

--- website/members/forms.py
+++ website/members/forms.py
@@ -4,5 +4,5 @@ from . import models
 class CreateMember(forms.ModelForm):
 	class Meta:
 		model = models.Members
-		fields=['name','year_of_graduation','position','branch','contact_details','experience']
+		fields=['name','year_of_graduation','position','branch','contact_details','experience','profile_picture']
 		
