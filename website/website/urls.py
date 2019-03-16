d6ece2fa3e7e15a4c1efb3a8f7f35500b99ce7af
ash1969 <nikhil.sonksbs@gmail.com>
2019-03-16 18:28:55 +0530

    Displaying Custom Error Page

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls import url, include
 from forum import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+
 urlpatterns = [
 
     path('admin/', admin.site.urls),
@@ -34,3 +36,6 @@ urlpatterns = [
     url(r'^markdownx/', include('markdownx.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = views.error_404
+handler500 = views.error_500
