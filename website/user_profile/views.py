b36833a7062940c52f730678c1c7dd357e31a343
pkkp86nitd <pkkumarpankaj86@gmail.com>
2019-03-20 17:47:21 +0530

    error fixing

--- website/user_profile/views.py
+++ website/user_profile/views.py
@@ -158,7 +158,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('edit_profile')
+            return redirect('profile:edit_profile')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
