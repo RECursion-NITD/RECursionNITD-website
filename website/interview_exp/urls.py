b921b2afa5422b2c69846cf705e45ae5d40971d8
ash1969 <nikhil.sonksbs@gmail.com>
2019-10-20 03:13:03 +0530

    Updating Experiences Allowed

--- website/interview_exp/urls.py
+++ website/interview_exp/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 app_name="experience"
 urlpatterns = [
       path('add', add_experience, name='add_experience'),
+      path('update/<int:id>/', update_experience, name='update_experience'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/interview_exp/views.py
+++ website/interview_exp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect,get_object_or_404, get_list_or_404
 from .models import *
 from .forms import ExperienceForm
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 
@@ -14,3 +15,18 @@ def add_experience(request):
             return redirect('interview_exp:list_experiences')
 
     return render(request, 'experience-form.html', {'form': form})
+
+@login_required
+def update_experience(request, id):
+    try:
+        experience = get_object_or_404(Experiences, pk=id)
+    except:
+        return HttpResponse("Content Does Not Exist :(")
+    else:
+        form = ExperienceForm(request.POST or None, instance = experience)
+
+        if form.is_valid():
+            form.save()
+            return redirect('interview_exp:list_questions')
+
+    return render(request, 'experience-form.html', {'form': form, 'experience': experience})
