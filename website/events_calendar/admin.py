88b7d688c06cdccb851426786540dae3b328a021
bishwajit <arin17bishwa@gmail.com>
2022-12-22 17:49:53 +0530

    revamped admin panel

--- website/events_calendar/admin.py
+++ website/events_calendar/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Events_Calendar)
+
+@admin.register(Events_Calendar)
+class EventsCalenderAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'venue')
+    list_filter = ('event_type', 'target_year',)
+    list_display = ('__str__', 'event_type', 'target_year', 'venue')
--- website/forum/admin.py
+++ website/forum/admin.py
@@ -1,35 +1,43 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 
 @admin.register(Questions)
 class QuestionsAdmin(admin.ModelAdmin):
-   #readonly_fields = ['created_at','updated_at','user_id']
-   pass
+    search_fields = ('title',)
+
 
 @admin.register(Tags)
 class TagAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Answers)
 class AnswerAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Taggings)
 class TaggingAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Comments)
 class CommentAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Upvotes)
 class UpvoteAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Follows)
 class FollowAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Comments_Answers)
 class Comments_AnswerAdmin(admin.ModelAdmin):
