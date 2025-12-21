48f70d1b238d0a3dc64eaadef960ddaac4984260
AayushAnand39 <aayushanand737@gmail.com>
2025-12-21 13:21:18 +0530

    Made a few backend changes to facilitate registration and user creation

--- website/website/settings.py
+++ website/website/settings.py
@@ -32,7 +32,13 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
