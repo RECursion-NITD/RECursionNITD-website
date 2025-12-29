f19084834eeaf4bc5ce85725bf1994f48f6822d9
Sandipan Kundu <kundu.sandipan18@gmail.com>
2025-12-29 21:32:37 +0530

    TEAMPAGE_update backend

--- website/team/urls.py
+++ website/team/urls.py
@@ -8,4 +8,8 @@ from django.conf.urls.static import static
 app_name="team"
 urlpatterns = [
     path('team_page/', team_page, name='team_page'),
+    path('alumni/', alumni_page, name='alumni'),
+    # API endpoints for React frontend
+    path('api/team/', team_api, name='team_api'),
+    path('api/alumni/', alumni_api, name='alumni_api'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/website/settings.py
+++ website/website/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'social_django',
     'widget_tweaks',
     'markdownx',
@@ -50,10 +51,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-     #'django.middleware.csrf.CsrfViewMiddleware',
+     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -182,6 +184,16 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'../website/media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True  # Only for development
 
 
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
